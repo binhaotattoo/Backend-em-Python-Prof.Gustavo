02ea6cd8eca3d97c993b26f5c612ffc277f1f59e
ANDRADE <julio.andrade@vidanova.local>
2022-10-25 21:51:03 -0300

    Adicionando Código parcial Modelagem Conta Digital

---
+++ Modelagem_Conta_Digital.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, Field
+import uvicorn
+from typing import Dict
+from fastapi import FastAPI
+from datetime import datetime
+
+app = FastAPI()
+
+accounts = []
+
+class Account(BaseModel):
+    id: int
+    nome: str
+    documento: str
+    dataNacimento: str
+    email: str
+    telefone: str
+
+@app.get("/")
+def root() -> str:
+    return "Jujubinha"
+
+@app.get("/health")
+def alive() -> Dict[str, datetime]:
+    return {"timestamp": datetime.now()}
+
+@app.post("/accounts", response_model=bool, tags=["accounts"])
+def create_account(account: Account):
+
+    accounts.append(account)
+    
+    return True
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
